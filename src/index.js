0449307a2ec2a2b9116b6b9feb3d23aeb04acf01
leoaristizabal <leonardo.aristizabal.m@gmail.com>
2023-07-25 16:59:13 -0300

    learning bout useState and UseEffect

--- src/index.js
+++ src/index.js
@@ -6,15 +6,12 @@ import App from "./App";
 const rootElement = document.getElementById('root');
 const root = ReactDOM.createRoot(rootElement);
 
-function AppTwo(){
-  return <h1>Esta es la App 2 solo para pruebas</h1>
-}
+
 
 root.render (
   <React.StrictMode>
   <>
     <App />
-    <AppTwo/>
   </>
   </React.StrictMode>,
   
