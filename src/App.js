bb2b25d16a54258713ee9018544fce9b429a0225
leoaristizabal <leonardo.aristizabal.m@gmail.com>
2023-07-27 17:39:13 -0300

    learning bout useReducer

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React, { useState, useEffect } from "react";
+import React, { useReducer} from "react";
 import './App.css';
 
 //Aqui renderizo todos los componenetes y activo los props que van en cada ccomponente
 function App() {
-  const [emocion, setEmocion] = useState("estresao");
-  const [secondary, setSecondary] = useState("Cansao");
   
-  useEffect (() => {
-    console.log(`!Es ${emocion} por aqui`);
-  });
-
-  useEffect (() => {
-    console.log(`!Es ${secondary} por aqui`);
-  });
+const [checked, toggle] = useReducer(
+  checked => !checked,
+  false
+  );
   
   return (
-    <> 
-    <h1>Hoy me siento como {emocion} y {secondary}</h1>
-    <button onClick={ () => setEmocion("Frustrao")}>Frustracion</button>
-    <button onClick={ () => setSecondary("Bellaco")}>Bellaco</button>
-    <button onClick={ () => setEmocion("Ladillao")}>Ladillao</button>
-    <button onClick={ () => setEmocion("Mejor")}>Mejor</button>
+    <>
+    <input type="checkbox" value= {checked} onChange={toggle} />
+    <p> {checked ? "Marcado" : "Sin Marcar"}</p>
     </>
   );
 }
