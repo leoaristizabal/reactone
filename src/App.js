a22edef9690d917e6b89b48a4a64cd39f64d7272
leoaristizabal <leonardo.aristizabal.m@gmail.com>
2023-07-31 09:36:59 -0300

    github api loading and error useState

--- src/App.js
+++ src/App.js
@@ -5,22 +5,34 @@ import './App.css';
 function App({login}) {
 
 const [datos, setDatos] = useState(null);
+const [cargando, setCargando] = useState(false);
+const [error, setError] = useState(null);
 
 useEffect(()=> {
+  if(!login) return;
+  setCargando(true);
   fetch(`https://api.github.com/users/${login}`)
   .then((response)=> response.json()
-  .then(setDatos));
-}, []);
+  .then(setDatos)
+  .then(()=> setCargando(false))
+  .catch(setError));
+}, [login]);
 
-if(datos) {
-  return <div>{JSON.stringify(datos)}</div>
+if(cargando) return <h1>Cargando...</h1>;
+if(error) return <pre>(JASON.stringify(error, null, 2))</pre>;
+if(!datos) return null;
+
+  return(
+  <div>
+  <h1>{datos.name}</h1>
+  <p>{datos.location}</p>
+  <img alt = {datos.login} src={datos.avatar_url}/>
+  </div>
+
+  )
+   
+    
 }
-  //54git
-  return (
-    <div>
-    No hay ningun usuario disponible
-    </div>
-  );
-}
+ 
 
 export default App;
