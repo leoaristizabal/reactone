645cfc964bd8ca9bcd62b344accea237d7cd3359
leoaristizabal <leonardo.aristizabal.m@gmail.com>
2023-08-06 20:44:21 -0300

    test learning with react library

--- src/App.js
+++ src/App.js
@@ -4,29 +4,11 @@ import './App.css';
 //Aqui renderizo todos los componenetes y activo los props que van en cada ccomponente
 function App({login}) {
 
-const [datos, setDatos] = useState(null);
-const [cargando, setCargando] = useState(false);
-const [error, setError] = useState(null);
-
-useEffect(()=> {
-  if(!login) return;
-  setCargando(true);
-  fetch(`https://api.github.com/users/${login}`)
-  .then((response)=> response.json()
-  .then(setDatos)
-  .then(()=> setCargando(false))
-  .catch(setError));
-}, [login]);
-
-if(cargando) return <h1>Cargando...</h1>;
-if(error) return <pre>(JASON.stringify(error, null, 2))</pre>;
-if(!datos) return null;
 
   return(
   <div>
-  <h1>{datos.name}</h1>
-  <p>{datos.location}</p>
-  <img alt = {datos.login} src={datos.avatar_url}/>
+  <h1>Hola, react testing library</h1>
+  
   </div>
 
   )
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,12 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import App from './App';
+import React from "react";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renderiza un h1 de prueba', () => {
+  const {getByText} = render (<App/>);
+  const h1 = getByText(/Hola React Testing Library/);
+  expect(h1).toHaveTextContent(
+    "Hola React Testing Library"
+  );
+
 });
